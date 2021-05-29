63473ccb9d1b963fc0618da92a73bcd98e989e98
Billu <billu@example.com>
2021-05-28 22:08:37 -0400

    Added app.java

--- JavaSchoolPrj/School/src/org/main/App.java
+++ JavaSchoolPrj/School/src/org/main/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int i = 0;
-        i = myFunctions.sumOftwo(3,4);
+        i = myFunctions.sumOftwo(3,7);
         System.out.println("Hello, World!- Sum of two number is " + i);
 
     }
