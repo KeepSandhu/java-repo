d2bd0726122c0a3c7bf8dfbc44b54647e93818b7
Billu <billu@example.com>
2021-05-28 21:29:35 -0400

    First time addedd java project

---
+++ JavaSchoolPrj/School/src/org/functions/myFunctions.java
@@ -0,0 +1,10 @@
+package org.functions;
+
+public class myFunctions {
+
+    public static int sumOftwo(int x, int y)
+    {
+        return x +y ;
+    }
+}
+
---
+++ JavaSchoolPrj/School/src/org/main/App.java
@@ -0,0 +1,14 @@
+package org.main;
+import org.functions.*;
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int i = 0;
+        i = myFunctions.sumOftwo(3,4);
+        System.out.println("Hello, World!- Sum of two number is " + i);
+
+    }
+}
